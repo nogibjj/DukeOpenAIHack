eca869ecacb138112007fa266962348b6649d6ac
Lorna <lorna1661maria@gmail.com>
2023-11-04 21:58:10 -0400

    added intro

--- src/generator_v2.py
+++ src/generator_v2.py
@@ -2,9 +2,11 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
 import json
 import openai
-from time import sleep
 
 # read openai api
 with open("../api_keys.json", 'r') as config_file:
@@ -16,15 +18,47 @@ openai.api_key = api_key
 # Initialize the OpenAIEmbeddings instance
 embeddings = OpenAIEmbeddings(openai_api_key=api_key)
 
-#load pregame information and add it to FAISS db
+# Load pregame information and add it to FAISS
 loader = TextLoader("../data/pregame.txt")
 documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 docs = text_splitter.split_documents(documents)
+main_db = FAISS.from_documents(docs, embeddings)
 
-db = FAISS.from_documents(docs, embeddings)
+# Load additional game information and add it to FAISS
+loader = TextLoader("../data/game_info.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs = text_splitter.split_documents(documents)
+intro_db = FAISS.from_documents(docs, embeddings)
 
+# Combine the two databases into a single database
+main_db.merge_from(intro_db)
+retriever = intro_db.as_retriever()
 
+def generate_game_intro(db):
+    retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .8, "k": 10})
+
+    prompt = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a commentator at an NBA match, answer the question based only on the following context:{retriever}"
+            },
+            {
+                "role": "user",
+                "content": "Generate an NBA game opener that a commentator would say"
+            }
+        ],
+        temperature=1,  # Adjust temperature as needed
+        max_tokens=256  # Adjust max_tokens as needed
+    )
+
+    game_intro = prompt.choices[0].message["content"]
+    return game_intro
+
+#print(generate_game_intro(intro_db))
 #get current line in game play json 
 def get_current_game_play():
     with open("../data/play_by_play.json", "r") as data_file:
@@ -56,7 +90,8 @@ def current_game_event(curr_event):
 # Function to retrieve the last n responses from the vector store
 def get_context_from_vector_store(curr_game_play_data):
     # Get the last n responses from the vector store
-    context = db.similarity_search(curr_game_play_data)
+    #context = db.similarity_search(curr_game_play_data)
+    context = main_db.as_retriever(curr_game_play_data, search_type="similarity_score_threshold", search_kwargs={"score_threshold": .8, "k": 10})
     return context
 
 
@@ -96,14 +131,19 @@ def add_to_vector_store(response):
     chunks = splitter.split_text(response)
 
     #add to vector store
-    db.add_texts(texts = chunks,embeddings= embeddings )
+    main_db.add_texts(texts = chunks,embeddings= embeddings )
     return None
 
 def main():
     data = get_current_game_play()
 
     n = 1
-    while n < 10:
+    while n < 5:
+
+        print("#"*100)
+        print(generate_game_intro(intro_db))
+        print("#"*100)
+
         event = data[n]
         curr_event = current_game_event(event)
         print(f"Current event: {curr_event}")
@@ -126,3 +166,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
